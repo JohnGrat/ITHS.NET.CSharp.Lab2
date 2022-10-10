bb766c9a7482db37949b700f1337bac26fa6e7d2
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2022-10-10 09:14:51 +0200

    minor update

--- Geometry/Sphere.cs
+++ Geometry/Sphere.cs
@@ -18,8 +18,8 @@ namespace Geometry
         public override ShapeType Shape => ShapeType.Sphere;
 
         public override Vector3 Center => _center;
-        public override float Area => MathF.PI * (MathF.Pow(_radius, 2));
-        public override float Volume => _radius * _radius * _radius;
+        public override float Area => (float)(4 * Math.PI * _radius * _radius);
+        public override float Volume => (float)(4.0 / 3 * Math.PI * _radius * _radius * _radius);
 
         public override string ToString() => $"Sphere @({_center.X:f2}, {_center.Y:f2}, {_center.Z:f2}): r = {_radius:f2}";
 
--- Labb2/Program.cs
+++ Labb2/Program.cs
@@ -4,6 +4,7 @@ using Geometry.Shapes;
 
 BaseShape[] shapes = new BaseShape[20];
 
+
 shapes = shapes.Select(x => x = BaseShape.GenerateShape()).ToArray();
 
 Shape3D[] shape3D = shapes.OfType<Shape3D>().ToArray();
@@ -12,6 +13,7 @@ var maxShape3D = shape3D.MaxBy(x => x.Volume);
 var triangleCrc = shapes.OfType<Triangle>().Sum(x => x.Circumference);
 float avgArea = shapes.Select(x => x.Area).Average();
 var avgShape = shapes.GroupBy(x => x.Shape).OrderByDescending(x => x.Count()).First();
+
 
 
 Console.ForegroundColor = ConsoleColor.White;
@@ -26,4 +28,7 @@ Console.WriteLine($"Total Triangle Circumference:{triangleCrc:f2}");
 Console.WriteLine($"3D Shape with largest volume is {maxShape3D.ToString()} and has volume: {maxShape3D.Volume:f2}");
 Console.WriteLine($"There most common shape is {avgShape.Key} and there are {avgShape.Count()} ");
 Console.ReadKey(true);
+
+
+
 
